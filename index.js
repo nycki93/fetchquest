234bb896273b21764f24bc55b263b5f6f021f2f6
nycki <nupanick@gmail.com>
2026-07-21 11:11:12 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require('node:fs/promises');
-const { Readable } = require('node:stream');
 const { JSDOM } = require('jsdom');
 
 async function fileExists(path) {
@@ -14,26 +13,23 @@ async function fileExists(path) {
   return true;
 }
 
-async function fetchQuest(path, uri) {
-  if (!await fileExists(path)) {
-    const res = await fetch(uri);
-    await fs.writeFile(path, Readable.fromWeb(res.body));
-  }
-}
-
-async function fetchImage(path, uri) {
+async function download(path, uri) {
   if (await fileExists(path)) return;
   const res = await fetch(uri);
   await fs.writeFile(path, res.body);
 }
 
-async function fetchQuest2(path, uri) {
+async function fetchQuest(path, uri) {
+  // download images as needed
+  await fs.mkdir(`${path}/res/`, { recursive: true });
+  const pending = [];
+
   let oldDom;
-  if (await fileExists(`${path}/quest-raw.html`)) {
-    oldDom = await JSDOM.fromFile(`${path}/quest-raw.html`);
+  if (await fileExists(`${path}/index-original.html`)) {
+    oldDom = await JSDOM.fromFile(`${path}/index-original.html`);
   } else {
     oldDom = await JSDOM.fromURL(uri);
-    await fs.writeFile(`${path}/quest-raw.html`, oldDom.serialize());
+    await fs.writeFile(`${path}/index-original.html`, oldDom.serialize());
   }
   const oldDoc = oldDom.window.document;
   const dom = new JSDOM(`
@@ -43,16 +39,12 @@ async function fetchQuest2(path, uri) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>quest</title>
-    <link rel="stylesheet" href="../style2.css">
+    <link rel="stylesheet" href="../style.css">
     </head>
     <body></body>
     </html>
   `);
   const doc = dom.window.document;
-
-  // download images as needed
-  await fs.mkdir(`${path}/res/`, { recursive: true });
-  const pending = [];
 
   // extract cover
   const cover = doc.createElement('div');
@@ -63,7 +55,7 @@ async function fetchQuest2(path, uri) {
   
   const coverImgLink = oldDoc.querySelector('#delform a:has(img)').getAttribute('href');
   const coverImgName = coverImgLink.split('/').at(-1);
-  pending.push(fetchImage(`${path}/res/${coverImgName}`, `https://questden.org${coverImgLink}`));
+  pending.push(download(`${path}/res/${coverImgName}`, `https://questden.org${coverImgLink}`));
   const coverImg = doc.createElement('img');
   coverImg.setAttribute('src', `./res/${coverImgName}`);
   coverImg.setAttribute('alt', coverImgName);
@@ -94,7 +86,7 @@ async function fetchQuest2(path, uri) {
     }
     
     const imgName = imgLink.split('/').at(-1);
-    pending.push(fetchImage(`${path}/res/${imgName}`, `https://questden.org${imgLink}`));
+    pending.push(download(`${path}/res/${imgName}`, `https://questden.org${imgLink}`));
 
     const post = doc.createElement('div');
     post.classList.add('post');
@@ -116,112 +108,11 @@ async function fetchQuest2(path, uri) {
   }
 
   await Promise.all(pending);
-  await fs.writeFile(`${path}/quest2.html`, dom.serialize());
-}
-
-async function fetchImages(path, posts) {
-  return Promise.all(posts.map(async (post) => {
-    const uri = post.imgSrc;
-    const name = uri.split('/').at(-1);
-    if (await fileExists(`${path}/${name}`)) return;
-    console.log(`fetching ${uri}`);
-    const res = await fetch(uri);
-    await fs.writeFile(`${path}/${name}`, res.body);
-  }));
-}
-
-function getCoverPage(dom) {
-  const form = dom.window.document.querySelector('#delform');
-  const imgSrc = `https://questden.org/${form.querySelector('a:has(img)').getAttribute('href')}`;
-  const title = form.querySelector('.filetitle').textContent.trim();
-  const author = form.querySelector('.postername').textContent.trim();
-  const desc = form.querySelector('blockquote').textContent.trim();
-  return { imgSrc, title, author, desc, id: 0 };
-}
-
-function * getPosts(dom) {
-  yield getCoverPage(dom);
-  let id = 1;
-  for (const el of dom.window.document.querySelectorAll('.reply')) {
-    const imgLink = el.querySelector('a:has(img)');
-    const imgSrc = imgLink && `https://questden.org${imgLink.getAttribute('href')}`;
-    const desc = el.querySelector('blockquote').textContent.trim();
-    yield { desc, id, imgSrc };
-    id += 1;
-  }
-}
-
-// TODO: set this up like the slime quest demo from nycki.net
-function pageHtml({ imgSrc, questTitle, title, desc, nextPage }) {
-  title = title ?? '==&gt;';
-  const pageTitle = (questTitle) ? `${questTitle} | ${title}` : title;
-
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width,initial-scale=1">
-      <title>${pageTitle}</title>
-      <link rel="stylesheet" href="../style.css">
-    </head>
-    <body>
-      <main>
-        <h1>${title}</h1>
-        <section>
-          <img src="${imgSrc}">
-          <p>
-            ${desc}
-          </p>
-        </section>
-      </main>
-      ${nextPage && `
-        <span class="next">&gt; <a href="${nextPage}">==&gt;</a></span>
-      `}
-      <nav>
-        <ul>
-          <li><a href="../p0">Start Over</a></li>
-          <li><a href="../p0">Go Back</a></li>
-        </ul>
-      </nav>
-    </body>
-    </html>
-  `;
+  await fs.writeFile(`${path}/index.html`, dom.serialize());
 }
 
 async function main() {
-  await fs.mkdir('_tmp', {recursive: true});
-  await fs.mkdir('_out', {recursive: true});
-  await fs.copyFile('style.css', '_out/style.css');
-
-  await fetchQuest('_tmp/quest.html', 'https://questden.org/kusaba/questarch/res/1002454.html');
-  const dom = await JSDOM.fromFile('_tmp/quest.html');
-
-  let questTitle = '';
-  const imagePosts = Array.from(getPosts(dom)).filter(p => p.imgSrc);
-  await fetchImages('_out', imagePosts);
-
-  for (let i = 0; i < imagePosts.length; i += 1) {
-    const { title, imgSrc, desc } = imagePosts[i];
-    if (!imgSrc) continue;
-    
-    const nextPage = (imagePosts[i+1] ? `../p${i+1}/` : null);
-    
-    const html = pageHtml({ questTitle, title, imgSrc, desc, nextPage });
-    await fs.mkdir(`_out/p${i}/`, { recursive: true });
-    await fs.writeFile(`_out/p${i}/index.html`, html);
-
-    if (!questTitle) {
-      questTitle = title;
-    }
-  }
-
-  console.log('ok.');
-  process.exit(0);
+  await fetchQuest('_out', 'https://questden.org/kusaba/quest/res/1129477.html');
 }
 
-async function main2() {
-  await fetchQuest2('_tmp', 'https://questden.org/kusaba/quest/res/1129477.html');
-}
-
-main2();
+main();
