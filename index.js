1010a59b626003b4910f47baf53bec5d203adeb8
nycki <nupanick@gmail.com>
2026-07-20 18:00:08 -0700

    next arrow

--- index.js
+++ index.js
@@ -23,43 +23,47 @@ function getCoverPage(dom) {
   const title = form.querySelector('.filetitle').textContent.trim();
   const author = form.querySelector('.postername').textContent.trim();
   const desc = form.querySelector('blockquote').textContent.trim();
-  return { imgSrc, title, author, desc, index: 0 };
+  return { imgSrc, title, author, desc, id: 0 };
 }
 
 function * getPosts(dom) {
   yield getCoverPage(dom);
-  let i = 1;
+  let id = 1;
   for (const el of dom.window.document.querySelectorAll('.reply')) {
     const imgLink = el.querySelector('a:has(img)');
     const imgSrc = imgLink && imgLink.getAttribute('href');
     const desc = el.querySelector('blockquote').textContent.trim();
-    yield { imgSrc, desc, index: i };
-    i += 1;
+    yield { imgSrc, desc, id };
+    id += 1;
   }
 }
 
 // TODO: set this up like the slime quest demo from nycki.net
-function pageHtml({ imgSrc, questTitle, title, desc }) {
+function pageHtml({ imgSrc, questTitle, title, desc, nextPage }) {
   const pageTitle = (questTitle) ? `${questTitle} | ${title}` : title;
+
   return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>${pageTitle}</title>
-  <link rel="stylesheet" href="../style.css">
-</head>
-<body>
-  <main>
-    <h1>${title}</h1>
-    <img src="${imgSrc}" height=10em>
-    <p>
-      ${desc}
-    </p>
-  </main>
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <title>${pageTitle}</title>
+      <link rel="stylesheet" href="../style.css">
+    </head>
+    <body>
+      <main>
+        <h1>${title}</h1>
+        <img src="${imgSrc}">
+        <p>
+          ${desc}
+        </p>
+        ${nextPage && `
+          <a class="next" href="${nextPage}">==&gt;</a>
+        `}
+      </main>
+    </body>
+    </html>
   `;
 }
 
@@ -72,18 +76,20 @@ async function main() {
   const dom = await jsdom.JSDOM.fromFile('_tmp/quest.html');
 
   let questTitle = '';
-  for (const p of getPosts(dom)) {
-    const { title, imgSrc, desc, index } = p;
+  const imagePosts = Array.from(getPosts(dom)).filter(p => p.imgSrc);
+  for (let i = 0; i < 2; i += 1) { // temporarily limited to 2 pages
+    const { title, imgSrc, desc } = imagePosts[i];
     if (!imgSrc) continue;
     
-    const html = pageHtml({ questTitle, title, imgSrc, desc });
-    await fs.mkdir(`_out/p${index}/`, { recursive: true });
-    await fs.writeFile(`_out/p${index}/index.html`, html);
+    const nextPage = (imagePosts[i+1] ? `../p${i+1}/` : null);
+    
+    const html = pageHtml({ questTitle, title, imgSrc, desc, nextPage });
+    await fs.mkdir(`_out/p${i}/`, { recursive: true });
+    await fs.writeFile(`_out/p${i}/index.html`, html);
 
     if (!questTitle) {
       questTitle = title;
     }
-    break;
   }
   
   console.log('ok.');
